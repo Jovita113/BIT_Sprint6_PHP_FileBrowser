ad618b5ec2458fa5598fa270312bb6db04b75de6
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-06-01 22:00:23 +0300

    Created admin area login form

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php session_start(); 	
+
+    $msg = ['username' => 'password', 'username1' => 'password1','username2' => 'password2'];
+	if(isset($_POST['Submit'])){
+		$logins = array('Jovita' => '123456', 'username1' => 'password1','username2' => 'password2');
+		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';		
+		if (isset($logins[$Username]) && $logins[$Username] == $Password){
+			$_SESSION['UserData']['Username']=$logins[$Username];
+			header("location:index.php");
+			exit;
+		} else {
+			$msg['username'] = "Wrong Username or Password";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <style><?php include 'css/login.css' ?></style>
+
+    <title>Admin Area</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    ADMIN PANEL
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form action=""<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" name="Login_Form">
+                            <div class="form-group">
+                                <label class="form-control-label">USERNAME</label>
+                                <input name="Username" type="text" class="form-control" placeholder="Jovita">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">PASSWORD</label>
+                                <input name="Password" type="password" class="form-control" placeholder="123456" i>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                            <?php if (isset($msg['username']) && $msg['username'] !== 'password'){?>
+                                <div class="col-lg-6 login-btm login-text">
+                                    <?php echo $msg['username']?>
+                                </div>
+                                <?php } ?>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <button name="Submit" type="submit" value="Login" class="btn btn-outline-primary">LOGIN</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+</body>
+</html>
