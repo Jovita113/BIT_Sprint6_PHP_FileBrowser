2d0f25bc5dc05d1c1b8d11bb461c514bd2c8ea8d
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-06-05 22:36:10 +0300

    Created folder creation form

--- index.php
+++ index.php
@@ -22,15 +22,19 @@ if (!isset($_SESSION['UserData']['Username'])) {
     <?php
     print('<h3 style="color:#FFF;">Congratulation! You have logged into password protected page.<a href="logout.php" style="color:yellow;">Click here</a> to Logout.</h3>');
     print('<br>');
-    print('<h2 style="color:#FFF; text-align:center;">File System Browser</h2>');
+    print('<h1 style="color:#82b74b; text-align:center;">File System Browser</h1>');
 
+    //folder and files list
     $path = isset($_GET["path"]) ? './' . $_GET["path"] : './';
     $files_and_dirs = scandir($path);
     print('<table><th>Type</th><th>Name</th><th>Actions</th>');
     foreach ($files_and_dirs as $fnd) {
         if ($fnd != ".." and $fnd != ".") {
             print('<tr>');
-            print('<td>' . (is_dir($path . $fnd) ? "Directory" : "File") . '</td>');
+            print('<td>' . (is_dir($path . $fnd)
+                ? '<img src= "./images/folder.png" class="dir">'
+                : '<img src= "./images/file.jpg" class="file">') 
+                . '</td>');
             print('<td>' . (is_dir($path . $fnd)
                 ? '<a href="' . (isset($_GET['path'])
                     ? $_SERVER['REQUEST_URI'] . $fnd . '/'
@@ -42,8 +46,26 @@ if (!isset($_SESSION['UserData']['Username'])) {
         }
     }
     print("</table>");
+    
+    //create new folder
+    $msgCreate = "Folder was created!";
+    $dir = $path;
+    if(isset($_REQUEST['path'])) {
+        $dir = urldecode($_REQUEST['path']);
+    }
+
+    if(isset($_REQUEST['createfolder'])) {
+        $foldername = trim($_REQUEST['createfolder']);    
+        if(mkdir($dir . "/" .$foldername, 0777, true)){        
+            header("location: ?path=" . $dir);
+        }
+    }
     ?>
+    <form action="" method="POST" >
+        <div class="createField">
+            <input type="text" name="createfolder" class="createInput">
+            <input type="submit" value="Create folder" class="createButton">
+        </div>
+    </form>
 </body>
-
 </html>
-
