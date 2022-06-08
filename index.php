13a80c963e1bdff2daceb5e48aa519f3881939b3
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-06-09 02:15:01 +0300

    Created download and delete button

--- index.php
+++ index.php
@@ -25,71 +25,97 @@ if (!isset($_SESSION['UserData']['Username'])) {
     print('<h3 class="logoutText";">Congratulation! You have logged into password protected page.<a href="logout.php" style="color:yellow;">Click here</a> to Logout.</h3>');
     print('<br>');
     print('<h1 style="color:#fff; text-align:center;">File System Browser</h1>');
- 
-    //create new folder
+
+    // creating a new folder
     $dir = "./";
     if (isset($_REQUEST['path'])) {
         $dir = urldecode($_REQUEST['path']);
     }
-
     if (isset($_REQUEST['createfolder'])) {
         $foldername = trim($_REQUEST['createfolder']);
-        if (mkdir($dir . "/" . $foldername, 0777, true)) {
-            print('<p class="warning">Directory was created!</p>');
-        } else if($foldername == false){
+        if ($foldername == false) {
             print('<p class="warning">Failed to create directory! Please write directory name</p>');
-        } else {
+            header("refresh:3");
+        } else if (mkdir($dir . "/" . $foldername)) {
+            print('<p class="warning">Directory was created!</p>');
+            header("refresh:3");
+        } else{
             print('<p class="warning">Such a directory already exists!</p>');
-        }
+            header("refresh:3");
+        } 
     }
-    ?>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <!-- create folder form -->
-        <div class="createField">
-            <input type="text" name="createfolder" class="createInput" />
-            <input type="submit" name="submit" value="Create folder" class="createButton" />
-        </div>
-    </form>
+    print(' 
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="createField">
+                    <input type="text" name="createfolder" class="createInput" />
+                    <input type="submit" name="submit" value="Create folder" class="createButton" />
+                </div>
+            </form>
+         ');
 
-    <?php
-    //upload files
-    if(isset($_FILES['image'])){
-        $errors= array();
+    // upload file logic
+    if (isset($_FILES['image'])) {
+        $errors = "";
         $file_name = $_FILES['image']['name'];
         $file_size = $_FILES['image']['size'];
         $file_tmp = $_FILES['image']['tmp_name'];
         $file_type = $_FILES['image']['type'];
-        $file_name_arr = explode('.', $_FILES['image']['name']);
-        $file_ext = strtolower(end($file_name_arr));
-        $extensions = array("jpeg","jpg","png");
-        if(in_array($file_ext,$extensions)=== false){
-
-            $errors[] = "Extension not allowed, please choose a JPEG or PNG file.";
+        $exploded = explode('.', $_FILES['image']['name']);
+        $file_ext = strtolower(end($exploded));
+        $extensions = ["jpeg", "jpg", "png"];
+        if (in_array($file_ext, $extensions) === false) {
+            $errors = '<p class="warningUpl">File format is not allowed, please choose a JPEG or PNG file.</p>';
+            header("refresh:3");
         }
-        if($file_size > 2097152) {
-            $errors[] ='File size must be smaller than 2 MB';
+        if ($file_size > 2097152) {
+            $errors = '<p class="warningUpl">File size must be smaller than 2 MB</p>';
+            header("refresh:3");
         }
-        if(empty($errors)==true) {
-            move_uploaded_file($file_tmp,"./" . $_GET["path"]. $file_name);
-            echo "Success";
-            header ("refresh:1");
-        }else{
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, "./" . $_GET['path']  . $file_name);
+            echo '<p class="warningUpl">Success!File uploaded</p>';
+            header("refresh:3");
+        } else{
             print_r($errors);
         }
-        print('<td>' .($_FILES['image']['name']) . '</td>');
     }
-    
-    ?>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <div class="uploadField">
-            <input type="file" name="image" class="uploadInput"/>
-            <input type="submit" value="Upload files" class="createButton"/>
-        </div>
-    </form>
+    print('
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="uploadField">
+                    <input type="file" name="image" class="uploadInput" />
+                    <input type="submit" value="Upload files" class="createButton" />
+                </div>
+            </form>
+        ');
 
-    <?php
-    //folder and files list
-    $path = isset($_GET["path"]) ? './' . $_GET["path"] : './';
+    // file download logic
+    if (isset($_POST['download'])) {
+        $file = './' . $_POST['download'];
+        $fileToDownloadEscaped = str_replace("&nbsp;", " ", htmlentities($file, 0, 'utf-8'));
+        ob_clean();
+        ob_start();
+        header('Content-Disposition: attachment; filename=' . basename($fileToDownloadEscaped));
+        header('Content-Length: ' . filesize($fileToDownloadEscaped));
+        ob_end_flush();
+        readfile($fileToDownloadEscaped);
+        exit;
+    }
+
+    // file delete logic
+    if (isset($_POST['delete'])) {
+        $file = './'  . $_POST['delete'];
+        if (file_exists($file)) {
+            unlink($file);
+            print('<p class="warningDlt">File is deleted</p>');
+        } else {
+            echo '<p class="warningDlt">File is not deleted!</p>';
+        }
+    }
+
+    // folder and files list
+    $path = isset($_GET["path"])
+        ? './' . $_GET["path"]
+        : './';
     $files_and_dirs = scandir($path);
     print('<table><th>Type</th><th>Name</th><th>Actions</th>');
     foreach ($files_and_dirs as $fnd) {
@@ -105,12 +131,27 @@ if (!isset($_SESSION['UserData']['Username'])) {
                     : $_SERVER['REQUEST_URI'] . '?path=' . $fnd . '/') . '">' . $fnd . '</a>'
                 : $fnd)
                 . '</td>');
-            print('<td></td>');
+            print('<td>' . (is_dir($path . $fnd)
+                ? '<form action="?path=' . $path . $fnd . '" method="post">
+                        <button class="downloadBtn" type="submit" name="download" value =' . $path . $fnd . '>DOWNLOAD</button>
+                   </form>'
+                : ($fnd === 'index.php'|| $fnd === 'login.php' || $fnd === 'logout.php' ||$fnd === 'Readme.md'
+                    ? '<form action="?path=' . $path . $fnd . '" method="post">
+                        <button class="downloadBtn" type="submit" name="download" value =' . $path . $fnd . '>DOWNLOAD</button>
+                       </form>'
+                    : '<form action="?path=' . $path . $fnd . '" method="post">
+                          <button class="downloadBtn" type="submit" name="download" value =' . $path . $fnd . '>DOWNLOAD</button>
+                       </form>' .
+                       '<form action="" method="post">
+                           <button class="deleteBtn" type="submit" name="delete" value =' . $path . $fnd . '>DELETE</button>
+                        </form>' )
+                )  
+                . '</td>');
             print('</tr>');
         }
     }
     print('</table>');
     ?>
-    
 </body>
+
 </html>
